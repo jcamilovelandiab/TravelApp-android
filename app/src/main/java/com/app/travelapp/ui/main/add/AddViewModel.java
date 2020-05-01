5c508f536dd34da0fd5236b3e0add2d94b643d89
camilo <jcamilovelandiab@gmail.com>
2020-04-30 20:37:59 -0500

    adding fragments with view models

---
+++ app/src/main/java/com/app/travelapp/ui/main/add/AddViewModel.java
@@ -0,0 +1,7 @@
+package com.app.travelapp.ui.main.add;
+
+import androidx.lifecycle.ViewModel;
+
+public class AddViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/app/travelapp/ui/main/search/DashboardFragment.java
@@ -0,0 +1,35 @@
+package com.app.travelapp.ui.main.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.app.travelapp.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel mViewModel;
+
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.add_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(DashboardFragment.class);
+        // TODO: Use the ViewModel
+    }
+}
